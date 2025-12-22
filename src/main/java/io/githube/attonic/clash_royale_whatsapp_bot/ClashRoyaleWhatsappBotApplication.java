5fa5dd52bb0f767a8daf72afc4ea0f0f1d036d0f
Antonio Cleison <a.cleisonn@gmail.com>
2025-12-22 18:25:16 -0300

    Commit inicial

---
+++ src/main/java/io/githube/attonic/clash_royale_whatsapp_bot/ClashRoyaleWhatsappBotApplication.java
@@ -0,0 +1,13 @@
+package io.githube.attonic.clash_royale_whatsapp_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClashRoyaleWhatsappBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClashRoyaleWhatsappBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/githube/attonic/clash_royale_whatsapp_bot/ClashRoyaleWhatsappBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.githube.attonic.clash_royale_whatsapp_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClashRoyaleWhatsappBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
